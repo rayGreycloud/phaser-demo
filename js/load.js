68b453dc0aa058d96d304eaa02e9c2646cc7e6f6
rayGreycloud <rayGreycloud@gmail.com>
2017-06-25 08:05:42 -0700

    Load game

---
+++ js/load.js
@@ -0,0 +1,18 @@
+// load.js
+
+let loadState = {
+  preload: function () {
+    var loadingLabel = game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#ffffff'});
+
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.PageAlignHorizonally = true;
+    game.scale.PageAlignVertically = true;
+    game.state.backgroundColor = '#000000';
+
+    game.load.spritesheet('characters', 'assets/sprites/charaters.png', 24, 24);
+
+  },
+  create: function () {
+    game.state.start('title');
+  }
+};
