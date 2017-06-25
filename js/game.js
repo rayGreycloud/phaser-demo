e89104d9926c1d1daaa731993e3f93991da7dd7c
rayGreycloud <rayGreycloud@gmail.com>
2017-06-25 07:52:33 -0700

    Add game initialization

---
+++ js/game.js
@@ -0,0 +1,23 @@
+// game.js
+// Set game parameters
+let config = {
+  width: 480,
+  height: 360,
+  renderer: 'Phaser.AUTO',
+  parent: 'gameDiv'
+  multiTexture: true,
+  antialias: true,
+  state: null
+}
+
+// Create game
+let game = new Phaser.Game(config);
+
+// Add states to state manager
+game.state.add('boot', bootState);
+game.state.add('load', loadState);
+game.state.add('title', titleState);
+game.state.add('play', playState);
+
+// Run boot state after initialization
+game.state.start('boot');
