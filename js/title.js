41ee99ffab23ce92d22c93593891fd3677edd966
rayGreycloud <rayGreycloud@gmail.com>
2017-06-26 11:29:35 -0700

    Skip title screen during dev

--- js/title.js
+++ js/title.js
@@ -3,17 +3,20 @@
 
 let titleState = {
   create: function () {
-    var nameLabel = game.add.text(160, 80, 'Click anywhere to start', {
-      font: '14px Space Mono',
-      fill: '#ffffff'
-    });
-    // Event listener
-    game.input.activePointer.capture = true;
+    // Comment out during dev
+    // var nameLabel = game.add.text(160, 80, 'Click anywhere to start', {
+    //   font: '14px Space Mono',
+    //   fill: '#ffffff'
+    // });
+    // // Event listener
+    // game.input.activePointer.capture = true;
+    // Start game
+    game.state.start('play');
   },
 
   update: function () {
-    if (game.input.activePointer.isDown) {
-      game.state.start('play');
-    }
+    // if (game.input.activePointer.isDown) {
+    //   game.state.start('play');
+    // }
   }
 };
