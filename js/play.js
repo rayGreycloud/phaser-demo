7191a88f748205dc440ef3eae8486ce2e456251b
rayGreycloud <rayGreycloud@gmail.com>
2017-06-26 20:24:47 -0700

    Refactor player and enemy into factory functions and set up collision

--- js/play.js
+++ js/play.js
@@ -1,29 +1,26 @@
 // play.js
 let playState = {
-  player: {
-    xDest: null,
-    yDest: null
-  },
-  enemy: {
-
-  },
+  player: null,
+  mob: null,
 
   create: function () {
     var _this = this;
-    _this.player = game.add.sprite(300, 200, 'characters');
-    _this.player.frame = 8;
+    _this.player = new Player(300, 200);
     game.add.existing(_this.player);
     game.physics.enable(_this.player, Phaser.Physics.ARCADE);
-    _this.player.anchor.setTo(.5, 1);
-    _this.player.animations.add('wait', [8,9], 4);
 
-    _this.enemy = game.add.sprite(100, 100, 'characters');
-    _this.enemy.frame = 544;
-    game.add.existing(_this.enemy);
-    game.physics.enable(_this.enemy, Phaser.Physics.ARCADE);
-    _this.enemy.anchor.setTo(.5, 1);
-    _this.enemy.scale.x = -1;
-    _this.enemy.body.immovable = true;
+    _this.mob = game.add.group();
+    _this.mob.add(Enemy(100, 100));
+    _this.mob.add(Enemy(200, 100));
+    _this.mob.add(Enemy(100, 200));
+    _this.mob.add(Enemy(200, 200));
+    _this.mob.add(Enemy(300, 300));
+    _this.mob.add(Enemy(400, 200));
+
+    _this.mob.forEach(function(enemy, index) {
+      game.physics.enable(enemy, Phaser.Physics.ARCADE);
+      enemy.body.immovable = true;
+    });
 
     game.input.activePointer.capture = true;
   },
@@ -31,47 +28,83 @@ let playState = {
   update: function () {
     var _this = this;
     _this.player.animations.play('wait');
-
-    if (game.input.activePointer.isDown) {
-      _this.player.xDest = game.input.x;
-      _this.player.yDest = game.input.y;
-    }
-
-    game.physics.arcade.collide(_this.player, _this.enemy, function () {
-      _this.stopPlayer();
+    _this.mob.forEach(function(enemy, index) {
+      enemy.animations.play('wait');
     });
 
-    _this.movePlayer(game.input.x, game.input.y);
-  },
-
-  movePlayer: function () {
-    var _this = this;
-
-    if (Math.floor(_this.player.x / 10) == Math.floor(_this.player.xDest / 10)) {
-      _this.player.body.velocity.x = 0;
-    } else if (Math.floor(_this.player.x) <  Math.floor(_this.player.xDest)) {
-      _this.player.body.velocity.x = 80;
-      _this.player.scale.x = -1;
-    } else if (Math.floor(_this.player.x) >  Math.floor(_this.player.xDest)) {
-      _this.player.body.velocity.x = -80;
-      _this.player.scale.x = 1;
+    if (game.input.activePointer.isDown) {
+      _this.player.setDest(game.input.x, game.input.y);
     }
-    if (Math.floor(_this.player.y / 10) == Math.floor(_this.player.yDest / 10)) {
-      _this.player.body.velocity.y = 0;
-    } else if (Math.floor(_this.player.y) <  Math.floor(_this.player.yDest)) {
-      _this.player.body.velocity.y = 80;
-      _this.player.scale.y = -1;
-    } else if (Math.floor(_this.player.y) >  Math.floor(_this.player.yDest)) {
-      _this.player.body.velocity.y = -80;
-      _this.player.scale.y = 1;
-    }
-  },
-
-  stopPlayer: function () {
-    var _this = this;
-    _this.player.xDest = _this.player.x;
-    _this.player.yDest = _this.player.y;
-    _this.player.body.velocity.x = 0;
-    _this.player.body.velocity.y = 0;
+    _this.player.update();
+    game.physics.arcade.collide(_this.player, _this.mob, function () {
+      _this.player.stop();
+    });
   }
 };
+
+function Player(x, y) {
+  var player = game.add.sprite(x, y, 'characters');
+  player.frame = 8;
+
+  player.xDest = x;
+  player.yDest = y;
+  player.anchor.setTo(.5, 1);
+  player.animations.add('wait', [8,9], 4);
+
+  player.setDest = function(x, y) {
+    player.xDest = x;
+    player.yDest = y;
+  };
+
+  player.update = function () {
+    var _this = this;
+    if (Math.floor(_this.x / 10) == Math.floor(_this.xDest / 10)) {
+      _this.body.velocity.x = 0;
+    } else if (Math.floor(_this.x) <  Math.floor(_this.xDest)) {
+      _this.body.velocity.x = 80;
+      _this.scale.x = -1;
+    } else if (Math.floor(_this.x) >  Math.floor(_this.xDest)) {
+      _this.body.velocity.x = -80;
+      _this.scale.x = 1;
+    }
+    if (Math.floor(_this.y / 10) == Math.floor(_this.yDest / 10)) {
+      _this.body.velocity.y = 0;
+    } else if (Math.floor(_this.y) <  Math.floor(_this.yDest)) {
+      _this.body.velocity.y = 80;
+    } else if (Math.floor(_this.y) >  Math.floor(_this.yDest)) {
+      _this.body.velocity.y = -80;
+    }
+  }
+
+  player.stop = function () {
+    var _this = this;
+    _this.xDest = _this.x;
+    _this.yDest = _this.y;
+    _this.body.velocity.x = 0;
+    _this.body.velocity.y = 0;
+  }
+
+  return player;
+};
+
+function Enemy(x, y) {
+  var enemy = game.add.sprite(100, 100, 'characters'); enemy.xDest = x;
+  enemy.yDest = y;
+  enemy.animations.add('wait', [544, 545], 4);
+
+  enemy.frame = 544;
+  enemy.anchor.setTo(.5, 1);
+  enemy.scale.x = -1;
+
+  enemy.goToXY = function(x, y) {
+
+  }
+  enemy.update = function () {
+
+  }
+  enemy.stop = function () {
+
+  }
+
+  return enemy;
+}
